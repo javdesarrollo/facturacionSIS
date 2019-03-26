de51bfc6baf565e76598175e101c5b3f1a6d7998
Administrador <Administrador@SISTEMAS06-PC>
2019-03-26 16:36:53 -0500

    tres

--- FacturacionSIS/src/Ventanas/Principal.java
+++ FacturacionSIS/src/Ventanas/Principal.java
@@ -61,7 +61,7 @@ public class Principal extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        JOptionPane.showMessageDialog(null,"LISTO CHULA");
+        JOptionPane.showMessageDialog(null,"LISTO CHULA ..:");
     }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
