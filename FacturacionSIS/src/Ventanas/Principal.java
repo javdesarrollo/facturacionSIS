169e9e4cf12bbf4cf0faefc773ab55c87a763096
Administrador <Administrador@SISTEMAS09-PC>
2019-03-26 16:32:57 -0500

    SEGUNDA

--- FacturacionSIS/src/Ventanas/Principal.java
+++ FacturacionSIS/src/Ventanas/Principal.java
@@ -5,6 +5,8 @@
  */
 package Ventanas;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Administrador
@@ -32,6 +34,11 @@ public class Principal extends javax.swing.JFrame {
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
         jButton1.setText("ok");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -52,6 +59,10 @@ public class Principal extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        JOptionPane.showMessageDialog(null,"LISTO CHULA");
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     /**
      * @param args the command line arguments
