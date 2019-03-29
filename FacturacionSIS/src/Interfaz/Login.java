58f41f9e76d95ed33cee48548e35d8891a511986
victor <victor@VICTOR>
2019-03-28 21:01:42 -0500

    bones rojo amarillo verde activacion

--- FacturacionSIS/src/Interfaz/Login.java
+++ FacturacionSIS/src/Interfaz/Login.java
@@ -14,7 +14,7 @@ public class Login extends javax.swing.JFrame {
         initComponents();
         super.setTitle("Acceso Sistema");
          setLocationRelativeTo(null);
-         btn_amari_activo.setVisible(false);
+         
        
     }
 
@@ -27,12 +27,9 @@ public class Login extends javax.swing.JFrame {
         txtPass = new javax.swing.JPasswordField();
         btnLogin = new javax.swing.JButton();
         btn_rojo = new javax.swing.JLabel();
-        btn_rojo_activo = new javax.swing.JLabel();
-        btn_verde = new javax.swing.JLabel();
-        btn_verde_activo = new javax.swing.JLabel();
-        btn_amari = new javax.swing.JLabel();
-        btn_amari_activo = new javax.swing.JLabel();
         Logo = new javax.swing.JLabel();
+        btn_verde = new javax.swing.JLabel();
+        btn_amarillo = new javax.swing.JLabel();
         Fondo = new javax.swing.JLabel();
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/elegante-formulario-iniciar-sesion_23-2147721241.jpg"))); // NOI18N
@@ -75,28 +72,48 @@ public class Login extends javax.swing.JFrame {
         });
         getContentPane().add(btnLogin, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 470, 180, 60));
 
-        btn_rojo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/btn_ext.png"))); // NOI18N
-        getContentPane().add(btn_rojo, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 10, -1, -1));
-
-        btn_rojo_activo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/btn_ext_activo.png"))); // NOI18N
-        btn_rojo_activo.setEnabled(false);
-        getContentPane().add(btn_rojo_activo, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 0, -1, -1));
-
-        btn_verde.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/btn_max.png"))); // NOI18N
-        getContentPane().add(btn_verde, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 10, -1, -1));
-
-        btn_verde_activo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/btn_max.png"))); // NOI18N
-        getContentPane().add(btn_verde_activo, new org.netbeans.lib.awtextra.AbsoluteConstraints(570, 10, -1, -1));
-
-        btn_amari.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/btn_mini.png"))); // NOI18N
-        getContentPane().add(btn_amari, new org.netbeans.lib.awtextra.AbsoluteConstraints(550, 10, -1, -1));
-
-        btn_amari_activo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/btn_mini_activo.png"))); // NOI18N
-        btn_amari_activo.setEnabled(false);
-        getContentPane().add(btn_amari_activo, new org.netbeans.lib.awtextra.AbsoluteConstraints(540, 0, -1, -1));
+        btn_rojo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/rojo_1.png"))); // NOI18N
+        btn_rojo.setToolTipText("");
+        btn_rojo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_rojoMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btn_rojoMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btn_rojoMouseExited(evt);
+            }
+        });
+        getContentPane().add(btn_rojo, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 40, 40));
 
         Logo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/jav fac.png"))); // NOI18N
         getContentPane().add(Logo, new org.netbeans.lib.awtextra.AbsoluteConstraints(220, 40, 200, 190));
+
+        btn_verde.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/verde.png"))); // NOI18N
+        btn_verde.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                btn_verdeMouseClicked(evt);
+            }
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btn_verdeMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btn_verdeMouseExited(evt);
+            }
+        });
+        getContentPane().add(btn_verde, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 0, 40, 40));
+
+        btn_amarillo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/Amarillo.png"))); // NOI18N
+        btn_amarillo.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseEntered(java.awt.event.MouseEvent evt) {
+                btn_amarilloMouseEntered(evt);
+            }
+            public void mouseExited(java.awt.event.MouseEvent evt) {
+                btn_amarilloMouseExited(evt);
+            }
+        });
+        getContentPane().add(btn_amarillo, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 0, 40, 40));
 
         Fondo.setFont(new java.awt.Font("Calibri Light", 0, 20)); // NOI18N
         Fondo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/elegante-formulario-iniciar-sesion_23-2147721241.jpg"))); // NOI18N
@@ -121,6 +138,45 @@ public class Login extends javax.swing.JFrame {
        dispose();
     }//GEN-LAST:event_btnLoginActionPerformed
 
+    private void btn_rojoMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_rojoMouseClicked
+         // TODO add your handling code here:
+        System.exit(0);        
+    }//GEN-LAST:event_btn_rojoMouseClicked
+
+    private void btn_rojoMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_rojoMouseEntered
+        btn_rojo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/rojoA_1.png")));
+    }//GEN-LAST:event_btn_rojoMouseEntered
+
+    private void btn_rojoMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_rojoMouseExited
+        // TODO add your handling code here:
+        btn_rojo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/rojo_1.png")));
+    }//GEN-LAST:event_btn_rojoMouseExited
+
+    private void btn_verdeMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_verdeMouseEntered
+        // TODO add your handling code here:
+       btn_verde.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/verdeA.png")));
+    }//GEN-LAST:event_btn_verdeMouseEntered
+
+    private void btn_verdeMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_verdeMouseExited
+        // TODO add your handling code here:
+       btn_verde.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/verde.png")));
+    }//GEN-LAST:event_btn_verdeMouseExited
+
+    private void btn_verdeMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_verdeMouseClicked
+        // TODO add your handling code here:
+        this.setExtendedState(ICONIFIED);
+    }//GEN-LAST:event_btn_verdeMouseClicked
+
+    private void btn_amarilloMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_amarilloMouseEntered
+        // TODO add your handling code here:
+        btn_amarillo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/AmarilloA.png")));
+    }//GEN-LAST:event_btn_amarilloMouseEntered
+
+    private void btn_amarilloMouseExited(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_btn_amarilloMouseExited
+        // TODO add your handling code here:
+         btn_amarillo.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Multimedia/Amarillo.png")));
+    }//GEN-LAST:event_btn_amarilloMouseExited
+
     public static void main(String args[]) throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException {
         UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
         Login in = new Login();
@@ -131,12 +187,9 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JLabel Fondo;
     private javax.swing.JLabel Logo;
     private javax.swing.JButton btnLogin;
-    private javax.swing.JLabel btn_amari;
-    private javax.swing.JLabel btn_amari_activo;
+    private javax.swing.JLabel btn_amarillo;
     private javax.swing.JLabel btn_rojo;
-    private javax.swing.JLabel btn_rojo_activo;
     private javax.swing.JLabel btn_verde;
-    private javax.swing.JLabel btn_verde_activo;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPasswordField txtPass;
     private javax.swing.JTextField txtUser;
